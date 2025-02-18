b7735cf926800f34e36c7bcdfbf3c77334dc6f51
Charles Morman <github@cmorman.com>
2025-02-18 15:27:58 -0700

    feat: define endpoint to delete a skill

--- app/routes/skills.py
+++ app/routes/skills.py
@@ -9,54 +9,74 @@ def list_all_skills():
     """List all skills."""
     return jsonify([skill.to_json() for skill in get_skill()])
 
+
 @skills_bp.route("/<int:skill_id>", methods=["GET"])
 def list_skill_by_id(skill_id):
     """Get a skill by ID."""
     skill = get_skill(skill_id=skill_id)
     return jsonify(skill.to_json())
 
+
 @skills_bp.route("/", methods=["POST"])
 def create_skill():
     """Create a new skill."""
-    
+
     # Get the data from the request
     data = request.json
-    
+
     # Check if data is provided
     if not data:
         return jsonify({"error": "No data provided"}), 400
-    
+
     # Check skill name is provided
     if not data.get("name").strip():
         return jsonify({"error": "Skill name cannot be empty"}), 400
     name = data.get("name").lower().strip()
-    
+
     # Check for duplicate skill name
     if get_skill(skill_name=name):
         return jsonify({"error": "Skill name already exists"}), 400
-    
+
     # Check if description is provided
     if data.get("description") == "" or data.get("description") is None:
         description = None
     else:
         description = data.get("description").strip()
-        
+
     # Add the new skill to the database
     skill = Skill(name=name, description=description)
     db.session.add(skill)
     db.session.commit()
-    
+
     # Return the created skill
-    return jsonify(
-        skill.to_json()
-    ), 201
+    return jsonify(skill.to_json()), 201
+
+
+@skills_bp.route("/<int:skill_id>", methods=["PUT"])
+def update_skill(skill_id):
+    pass
+
+
+@skills_bp.route("/<int:skill_id>", methods=["DELETE"])
+def delete_skill(skill_id):
+    """Delete a skill by ID"""
+    skill = get_skill(skill_id=skill_id)
+
+    if skill:
+        db.session.delete(skill)
+        db.session.commit()
+        return jsonify({"message": "Skill deleted successfully"}), 200
+    return jsonify({"error": "Skill not found"}), 404
+
 
 def get_skill(skill_id=None, skill_name=None):
     """Get all skills or a specific skill by ID or name."""
-    
+
     # Return an error if both skill_id and skill_name are provided
     if skill_id and skill_name:
-        return jsonify({"error": "Please provide either a skill ID or a skill name, not both"}), 400
+        return jsonify(
+            {"error": "Please provide either a skill ID or a skill name, not both"}
+        ), 400
     # Get skill by ID if provided
     elif skill_id:
         return Skill.query.get(skill_id)
@@ -65,4 +85,3 @@ def get_skill(skill_id=None, skill_name=None):
         return Skill.query.filter_by(name=skill_name).first()
     # Get all skills if no ID or name is provided
     return Skill.query.all()
-
