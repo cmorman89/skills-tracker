0eb7e843f2d156596bd81cecddb8c7c9ff134526
Charles Morman <github@cmorman.com>
2025-02-24 21:40:08 -0700

    feat: update onClick of button components ref #1, #4

--- frontend/src/TextWithPlus.jsx
+++ frontend/src/TextWithPlus.jsx
@@ -2,29 +2,29 @@ import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 
-const TextWithPlus = ({ text, onClick }) => {
+const TextWithPlus = ({ skill, text, onClick }) => {
     return (
         <div
             className="flex border border-teal-800 flex-wrap items-center w-fit m-2 pl-3 bg-slate-700 rounded-md shadow-lg justify-between hover:border-teal-400/50 h-10"
+            onClick={() => onClick(skill)}
             >
             <small className="pr-3">
                 {text}
             </small>
             <div className="bg-teal-800/50 flex items-center justify-center h-full p-2">
-                <a onClick={() => onClick(text)}>
                     <FontAwesomeIcon
                         icon={faPlusCircle}
                         className="text-white/50 hover:text-white/80"
-                    />
-                </a>    
+                    /> 
             </div>
         </div>
     );
 }
 
 TextWithPlus.propTypes = {
-    text: PropTypes.string.isRequired,
     onClick: PropTypes.func,
+    skill: PropTypes.object,
+    text: PropTypes.string.isRequired,
 };
 
 TextWithPlus.defaultProps = {
--- frontend/src/TextWithX.jsx
+++ frontend/src/TextWithX.jsx
@@ -2,21 +2,20 @@ import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 
-const TextWithX = ({ text, onClick }) => {
+const TextWithX = ({ skill, text, onClick }) => {
     return (
         <div
-            className="flex border border-amber-800 flex-wrap items-center w-fit m-2 pl-3 bg-slate-700 rounded-md shadow-lg justify-between hover:border-orange-400/50 h-10"
-            >
+            className="flex border border-orange-400/50 flex-wrap items-center w-fit m-2 pl-3 bg-slate-700 rounded-md shadow-lg justify-between hover:border-orange-400/80 h-10"
+            onClick={() => onClick(skill)}
+        >
             <small className="pr-3">
                 {text}
             </small>
             <div className="bg-orange-400/50 flex items-center justify-center h-full p-2">
-                <a onClick={() => onClick(text)}>
-                    <FontAwesomeIcon
-                        icon={faCircleXmark}
-                        className="text-white/50 hover:text-white/80"
-                    />
-                </a>    
+                <FontAwesomeIcon
+                    icon={faCircleXmark}
+                    className="text-white/50 hover:text-white/80"
+                />
             </div>
         </div>
     );
@@ -24,8 +23,9 @@ const TextWithX = ({ text, onClick }) => {
 
 
 TextWithX.propTypes = {
-    text: PropTypes.string.isRequired,
     onClick: PropTypes.func,
+    skill: PropTypes.object,
+    text: PropTypes.string.isRequired,
 };
 
 TextWithX.defaultProps = {
