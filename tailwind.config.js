1f046fad1d32cc5150d2deebe12b0047676bcad7
Charles Morman <github@cmorman.com>
2025-02-25 12:11:39 -0700

    chore: remove unused files, fix requirement reqressions, etc

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./app/templates/**/*.{html,js}", "./app/static**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
-
