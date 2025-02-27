916b855bb4d18b4c7e23093dcac10dda25383325
Charles Morman <github@cmorman.com>
2025-02-27 14:49:45 -0700

    feat: add backend route for finding source by name ref #5

--- app/routes/sources.py
+++ app/routes/sources.py
@@ -5,7 +5,7 @@ from ..models import Source, db
 # Define the blueprint for sources
 sources_bp = Blueprint("sources", __name__, url_prefix="/api/v1/sources")
 
-
+# FETCHING SOURCES
 @sources_bp.route("/", methods=["GET"])
 def list_all_sources():
     """List all sources."""
@@ -18,6 +18,16 @@ def get_source(source_id):
     source = Source.query.get_or_404(source_id)
     return jsonify(source.to_json())
 
+@sources_bp.route("/<string:source_name>", methods=["GET"])
+def get_source_by_name(source_name):
+    """Get a source by name."""
+    source_name = source_name.strip().lower()
+    source = Source.query.filter_by(name=source_name).first()
+    if not source:
+        return jsonify()
+    return jsonify(source.to_json())
+
+# CREATING SOURCES
 @sources_bp.route("/", methods=["POST"])
 def create_source():
     """Create a new source."""
@@ -26,7 +36,7 @@ def create_source():
         return Response("Invalid input", status=400)
 
     new_source = Source(
-        name=data.get("name"),
+        name=data.get("name").strip().lower(),
         description=data.get("description"),
         type=data.get("type"),
     )
