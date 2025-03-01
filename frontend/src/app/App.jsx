ec7c5330ff6ff9ea3cf9a34d7cd7a32f53e1910c
Charles Morman <github@cmorman.com>
2025-03-01 13:03:13 -0700

    feat: add message area to UI

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Sidebar from "../features/core/sidebar/components/Sidebar";
 import MainContent from "../features/core/main_content/components/MainContent";
+import MessageContent from "../features/core/message_content/components/MessageContent";
 
 
 const App = () => {
@@ -8,7 +9,10 @@ const App = () => {
         <Router>
             <div className="flex min-h-screen px-8 py-12 gap-8">
                 <Sidebar />
-                <MainContent />
+                <div className="flex flex-col flex-grow gap-8">
+                    <MessageContent />
+                    <MainContent />
+                </div>
             </div>
         </Router>
     );
---
+++ frontend/src/features/core/message_content/components/MessageContent.jsx
@@ -0,0 +1,11 @@
+const MessageContent = () => {
+    
+    return (
+        <div className="flex flex-col bg-green-300/15 rounded-xl shadow-2xl border border-green-700/60 px-8 py-2 ease-in-out duration-300 text-green-300">
+            <div className="">
+                Success or failure messages to appear here
+            </div>
+        </div>
+    )
+}
+export default MessageContent;
