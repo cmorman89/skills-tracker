c4c6f142cb1f48aef2e4158352881d99ab7de308
Charles Morman <github@cmorman.com>
2025-03-04 21:15:23 -0700

    feat: adjust background color

--- frontend/src/features/core/main_content/components/MainContent.jsx
+++ frontend/src/features/core/main_content/components/MainContent.jsx
@@ -5,7 +5,7 @@ const MainContent = ({ innerComponent, pageTitle }) => {
 
     return (
 
-        <div className="flex flex-col flex-grow bg-gradient-to-b from-neutral-200 to-fuchsia-200 p-8">
+        <div className="flex flex-col flex-grow bg-gradient-to-b from-neutral-200 to-fuchsia-100 p-8">
             <h1 className="pageTitle mb-4">{pageTitle}</h1>
             {
                 <div className="mb-4">
